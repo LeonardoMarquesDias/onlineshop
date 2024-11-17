f9d98e96af91a515b0a365b936b4a17b8fefb44e
LeonardoMarquesDias <sofrontend@gmail.com>
2024-11-17 03:58:11 +0000

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,14 @@ module.exports = {
       screens: {
         'iPhone11Pro': '375px',
         'iPhone12ProMax': '379px',
-        'iPhone13mini': '360px',
+        'iPhone13mini': '340px',
         'iPhone13Pro': '428px',
         'iPhone15Pro': '393px',
         'iPhone15ProMax': '430px',
-
         'iPadmini': '608px',
         'iPad': '575px',
         'iPadPro11': '581px',
         'iPadAir5': '820x',
-
-
         'MacbookAirM2': '1280px',
         'MacbookPro16': '1728px',
         'iMac24': '1120px',
