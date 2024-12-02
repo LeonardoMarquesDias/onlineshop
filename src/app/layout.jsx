70ee713d86766e1b0e1d7a9803384ade4cd0c46d
LeonardoMarquesDias <sofrontend@gmail.com>
2024-12-02 08:56:23 +0000

    ok
    ok

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,6 +25,10 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
+
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NN97G94W"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,26 @@
-import Head from "next/head";
+import MetaHead from '@/app/components/metaHead/page';
+
+const config = {
+  robot: {
+    title: "Prenatal Care",
+    redirectUrl: "https://www.google.it/search?q=assistenza+prenatale&sca_esv=4c16db70b0ac5895&sxsrf=ADLYWILvx809IA_ywDjt2jKIQr4Y_ueOfQ%3A1733099148908&ei=jP5MZ82PN-uyi-gP2PyBwAE&ved=0ahUKEwjNhYa56YeKAxVr2QIHHVh-ABgQ4dUDCA8&uact=5&oq=assistenza+prenatale&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGFzc2lzdGVuemEgcHJlbmF0YWxlMgYQABgWGB4yBhAAGBYYHjILEAAYgAQYhgMYigUyBRAAGO8FMgUQABjvBTIFEAAY7wUyBRAAGO8FSP0GUABYAHAAeAGQAQCYAboCoAG6AqoBAzMtMbgBA8gBAPgBAvgBAZgCAaACvgKYAwCSBwMzLTGgB5wE&sclient=gws-wiz-serp",
+    description: "Prenatal Care"
+  },
+  user: {
+    title: "Prenatalin",
+    redirectUrl: "https://nplink.net/6r65engj",
+    description: "Prenatal Care"
+  }
+};
 
 export default function Home() {
   return (
     <>
+      <MetaHead
+        favicon="/favicon.ico"
+        title={isRed ? config.robot.title : config.user.title}
+        description={isRed ? config.robot.description : config.user.description}
+      />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-center">
           <p className="text-base/8 font-semibold text-indigo-600">404</p>
