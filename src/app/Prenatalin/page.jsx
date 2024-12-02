77f1f38b01d2494c7f3210facf132b1e187d4875
LeonardoMarquesDias <sofrontend@gmail.com>
2024-12-02 10:47:36 +0000

    tag
    taf

--- src/app/Prenatalin/page.jsx
+++ src/app/Prenatalin/page.jsx
@@ -83,14 +83,14 @@ export default function PreNatalin() {
 
   return (
     <> 
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16804390453">
-      </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16804390453"></script>
       <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-16804390453');
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16804390453');
+        `}
       </script>
       <MetaHead
         favicon="/favicon.ico"
