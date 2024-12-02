515afb7871dc930792f088b1d5f0697643f6362a
LeonardoMarquesDias <sofrontend@gmail.com>
2024-12-02 09:01:07 +0000

    ok
    ok

--- src/app/components/metaHead/page.jsx
+++ src/app/components/metaHead/page.jsx
@@ -3,17 +3,6 @@ import Head from 'next/head';
 const MetaHead = ({ title, description, favicon }) => {
   return (
     <Head>
-      {/* Google Tag Manager */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NN97G94W');`,
-        }}
-      />
-      {/* End Google Tag Manager */}
       <title>{title}</title>
       <meta name="description" content={description} />
       {favicon && <link rel="icon" href={favicon} type="image/png" />}
@@ -22,4 +11,3 @@ const MetaHead = ({ title, description, favicon }) => {
 };
 
 export default MetaHead;
-
