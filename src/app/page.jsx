841d4b119fa54a7f38fb34b91a99cb4cf96d66d5
LeonardoMarquesDias <sofrontend@gmail.com>
2024-12-05 06:25:31 +0000

    ok
    ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,70 @@
+"use client";
+
+import Link from 'next/link';
 import MetaHead from '@/app/components/metaHead/page';
 
-const config = {
-  robot: {
-    title: "Prenatal Care",
-    redirectUrl: "https://www.google.it/search?q=assistenza+prenatale&sca_esv=4c16db70b0ac5895&sxsrf=ADLYWILvx809IA_ywDjt2jKIQr4Y_ueOfQ%3A1733099148908&ei=jP5MZ82PN-uyi-gP2PyBwAE&ved=0ahUKEwjNhYa56YeKAxVr2QIHHVh-ABgQ4dUDCA8&uact=5&oq=assistenza+prenatale&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGFzc2lzdGVuemEgcHJlbmF0YWxlMgYQABgWGB4yBhAAGBYYHjILEAAYgAQYhgMYigUyBRAAGO8FMgUQABjvBTIFEAAY7wUyBRAAGO8FSP0GUABYAHAAeAGQAQCYAboCoAG6AqoBAzMtMbgBA8gBAPgBAvgBAZgCAaACvgKYAwCSBwMzLTGgB5wE&sclient=gws-wiz-serp",
-    description: "Prenatal Care"
-  },
-  user: {
-    title: "Prenatalin",
-    redirectUrl: "https://nplink.net/6r65engj",
-    description: "Prenatal Care"
-  }
+const backgrounds = {
+  iPhone11ProBg: "iPhone11Pro:bg-[url('/cookies/iPhone11Pro.png')]",
+  iPhone12ProMaxBg: "iPhone12ProMax:bg-[url('/cookies/iPhone12ProMax.png')]",
+  iPhone13miniBg: "iPhone13mini:bg-[url('/cookies/iPhone13mini.png')]",
+  iPhone13ProBg: "iPhone13Pro:bg-[url('/cookies/iPhone13Pro.png')]",
+  iPhone15ProBg: "iPhone15Pro:bg-[url('/cookies/iPhone15Pro.png')]",
+  iPhone15ProMaxBg: "iPhone15ProMax:bg-[url('/cookies/iPhone15ProMax.png')]",
+  iPadminiBg: "iPadmini:bg-[url('/cookies/iPadmini.png')]",
+  iPadBg: "iPad:bg-[url('/cookies/iPad.png')]",
+  iPadPro11Bg: "iPadPro11:bg-[url('/cookies/iPadPro11.png')]",
+  iPadAir5Bg: "iPadAir5:bg-[url('/cookies/iPadAir5.png')]",
+  MacbookAirM2Bg: "MacbookAirM2:bg-[url('/cookies/MacbookAirM2.png')]",
+  MacbookPro16Bg: "MacbookPro16:bg-[url('/cookies/MacbookPro16.png')]",
+  iMac24Bg: "iMac24:bg-[url('/cookies/iMac24.png')]",
+  MacbookAirBg: "MacbookAir:bg-[url('/cookies/MacbookAir.png')]",
+  LaptopBg: "Laptop:bg-[url('/cookies/Laptop.png')]",
+  Desktop1Bg: "Desktop1:bg-[url('/cookies/Desktop1.png')]",
+  Desktop2Bg: "Desktop2:bg-[url('/cookies/Desktop2.png')]",
 };
 
-export default function Home() {
+export default function Cookie() {
+
   return (
     <>
       <MetaHead
         favicon="/favicon.ico"
-        title={isRed ? config.robot.title : config.user.title}
-        description={isRed ? config.robot.description : config.user.description}
+        title="Cookie"
+        description="Policy"
       />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-center">
-          <p className="text-base/8 font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-pretty text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
-            Page not found
-          </h1>
-          <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-        </main>
-      </div>
+
+      <main className={`bg-cover h-screen flex justify-center items-center relative ${Object.values(backgrounds).join(' ')}`}>
+        <div className="absolute inset-0 bg-black opacity-70 z-10"></div>
+
+        {/* Banner de Cookie */}
+        <div className="fixed bottom-0 left-0 right-0 w-full bg-white text-gray-800 shadow-xl p-6 sm:p-8 lg:p-10 rounded-t-lg z-50">
+          <div className="flex flex-col sm:flex-row justify-between items-center">
+            <div className="flex flex-col sm:w-4/5 text-center sm:text-left">
+              <h2 className="text-xl sm:text-2xl font-semibold mb-2">Politica sui Cookie</h2>
+              <p className="text-sm text-justify sm:text-base mb-4">
+                Utilizziamo i cookie per migliorare la tua esperienza. Continuando a navigare, acconsenti all'uso dei cookie. Per ulteriori informazioni, consulta la nostra:   
+                <br/><span className="text-blue-600 hover:text-blue-800"> Politica sui Cookie </span>
+              </p>
+            </div>
+            <div className="flex flex-row justify-center space-x-4 mt-4 sm:mt-0">
+              <Link href="">
+                <button 
+                  className="w-full border-2 border-blue-600 sm:w-auto bg-blue-600 text-white py-1.5 px-5 sm:py-2 sm:px-6 rounded-md hover:bg-blue-700 transition-all"
+                >
+                  Accetta
+                </button>
+              </Link>
+              <Link href="">
+                <button 
+                  className="w-full sm:w-auto bg-transparent border-2 border-gray-500 text-gray-700 py-1.5 px-5 sm:py-2 sm:px-6 rounded-md hover:bg-gray-100 hover:border-gray-600 transition-all"
+                >
+                  Rifiuta
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
